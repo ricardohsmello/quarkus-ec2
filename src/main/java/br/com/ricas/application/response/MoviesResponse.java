e8be89745545b7ab63577493993dbfdb88a854f6
Ricardo <ricardohsmello@gmail.com>
2023-10-04 01:09:21 -0300

    refactor

--- src/main/java/br/com/ricas/application/response/MoviesResponse.java
+++ src/main/java/br/com/ricas/application/response/MoviesResponse.java
@@ -2,12 +2,52 @@ package br.com.ricas.application.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
+
 public class MoviesResponse {
+    private String _id;
+    private String titulo;
+    private String descricao;
+    private List<String> elenco;
 
-    private String title;
+    // Constructors, getters, and setters
 
-    public String getTitle() {
-        return title;
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<String> getElenco() {
+        return elenco;
+    }
+
+    public void setElenco(List<String> elenco) {
+        this.elenco = elenco;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
+

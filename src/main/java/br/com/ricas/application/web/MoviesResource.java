a123158e41bbd61c1317d4ef1fa2c69907a6fd53
Ricardo <ricardohsmello@gmail.com>
2023-10-04 01:09:17 -0300

    refactor

--- src/main/java/br/com/ricas/application/web/MoviesResource.java
+++ src/main/java/br/com/ricas/application/web/MoviesResource.java
@@ -1,11 +1,16 @@
 package br.com.ricas.application.web;
 
+ import br.com.ricas.application.response.MoviesResponse;
 import br.com.ricas.domain.service.MoviesService;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/movies")
 public class MoviesResource {
@@ -13,10 +18,13 @@ public class MoviesResource {
     @RestClient
     MoviesService moviesService;
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getMovies() {
+    @Inject
+    @ConfigProperty(name = "mongodb.http.api")
+    String apiKey;
 
-        return moviesService.getMovies().getTitle();
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<MoviesResponse> getMovies() {
+        return moviesService.getMovies(apiKey);
     }
 }

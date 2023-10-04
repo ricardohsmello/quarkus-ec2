086e86ad331d98e7edefeb93e60e1b9a60b9cbbe
Ricardo <ricardohsmello@gmail.com>
2023-10-04 01:09:23 -0300

    refactor

--- src/main/java/br/com/ricas/domain/service/MoviesService.java
+++ src/main/java/br/com/ricas/domain/service/MoviesService.java
@@ -3,14 +3,16 @@ package br.com.ricas.domain.service;
 import br.com.ricas.application.response.MoviesResponse;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @RegisterRestClient(configKey = "movies")
 public interface MoviesService {
     @GET
     @Path("/app/application-0-qobua/endpoint/movies")
     @Produces(MediaType.APPLICATION_JSON)
-    MoviesResponse getMovies();
+    List<MoviesResponse> getMovies(@HeaderParam("apiKey") String apiKey);
 }
